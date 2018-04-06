70853f66233c39a5d22ea12c946789fddd821553
Roman Sineokov <rsineokov@life-team.net>
2018-04-06 11:16:05 +0300

    start to add redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from 'components/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'styles/index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import rootReducer from './reducers';
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
